a85061abd2090ade81b99712fad80822e428f5dc
Catarina Laranjeiro <catarina@studiographene.com>
2025-03-11 09:37:43 +0000

    chore: clears comments and support links from README in main branch after creating new db-typeorm branch

--- src/database/migrations/1741624462058-SeedTablesData.ts
+++ src/database/migrations/1741624462058-SeedTablesData.ts
@@ -1,15 +1,7 @@
-/**
- * This is a migration file that seeds data into the tables
- * Empty migration file was created with script command "migration:create ./src/database/migrations/SeedTablesData"
- * which generated a file with empty up and down methods
- * We can use TypeORM QueryBuilder to insert or delete data into the tables and more (check docs)
- */
-
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SeedTablesData1741624462058 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // TypeORM QueryBuilder
     await queryRunner.manager
       .createQueryBuilder()
       .insert()
@@ -32,12 +24,6 @@ export class SeedTablesData1741624462058 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    /**
-     * Migration can be reverted with script command "migration:revert"
-     * A migration table was created in the DB and lists all migrations that have been run
-     * Above command reverts the last entry in the table. If last entry is related to this one, 
-     * it will revert the seed data in above up method
-     */
     await queryRunner.manager
       .createQueryBuilder()
       .delete()
